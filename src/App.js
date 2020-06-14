48fc136e9b670c6d444c746c01e3b5422dca7afb
Muhammad Shariq Bilal <shariqghori01@gmail.com>
2020-06-13 18:57:40 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Prayers from './prayers';
+
+function App() {
+  return ( <div className="App">
+    <header className="App-header">
+      <h1> Assalam O Alaikum..! </h1>
+      <Prayers prayerName="Ameen" prayerName1="ALLHAMDULILLAH"/>
+
+      {/* <Prayers></Prayers> */}
+
+      
+    </header>
+  </div>);
+  
+}
+
+export default App;
+
+var student = {
+  name: 'Shariq',
+  course: 'Bootcamp2020'
+
+}
+
---
+++ src/prayers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Prayers (props){
+    return ( 
+        <div>
+        <h1> May Allah Blessed You..! {props.prayerName} </h1>
+
+        <hr/>
+
+        <h1> We Always Say {props.prayerName1} </h1>
+        </div>
+
+    )
+}
+
+export default Prayers
