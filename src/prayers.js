575cf53d13e4ed8cd0b2917e1d09724b91afc6b1
Muhammad Shariq Bilal <shariqghori01@gmail.com>
2020-06-13 22:49:01 -0700

    Update js Files

--- src/prayers.js
+++ src/prayers.js
@@ -3,6 +3,8 @@ import React from 'react';
 function Prayers (props){
     return ( 
         <div>
+        <h1> I am {props.name}</h1>
+        <p> "This is my 3rd Assignment" </p>
         <h1> May Allah Blessed You..! {props.prayerName} </h1>
 
         <hr/>
